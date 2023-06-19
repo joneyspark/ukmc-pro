8463e6ec146978d613e614549011150dfb833eef
khantanvir <aiub.tanvir@gmail.com>
2023-06-19 13:50:45 +0600

    sddd
    Signed-off-by: khantanvir <aiub.tanvir@gmail.com>

--- resources/views/application/processing.blade.php
+++ resources/views/application/processing.blade.php
@@ -132,30 +132,44 @@
                                         <div role="menu" class="" data-toggle="">Meeting</div>
                                     </section>
                                 </div>
-                                <div>
-                                    <div class="card-body custom-card-body p-0">
-                                        <div class="col-col-md-12">Meeting</div><br>
-                                        
-                                        <div data-v-b64104d7="" id="confirmationModal" class="modal confirmation-custom fade"
-                                            tabindex="-1" role="dialog" aria-labelledby="confirmationModalLabel"
-                                            aria-hidden="true">
-                                            <div data-v-b64104d7="" class="modal-dialog" role="document">
-                                                <div data-v-b64104d7="" class="modal-content">
-                                                    <div data-v-b64104d7="" class="modal-header">
-                                                        <h5 data-v-b64104d7="" id="confirmationModalLabel" class="modal-title">
-                                                            Confirmation</h5><button data-v-b64104d7="" type="button"
-                                                            class="close" data-dismiss="modal" aria-label="Close"></button>
+                                <div id="" class="" aria-labelledby="headingTwo2" data-parent="#withoutSpacing">
+                                    <div id="meetingnote-data">
+                                        @if(count($application_info->meetings) > 0)
+                                            @foreach ($application_info->meetings as $note)
+                                            <div class="row col-md-12 mt-3" id="">
+                                                <div style="margin-left:7px;" class="media custom-media-img">
+                                                    <div style="margin-left: 2px;" class="ml-2">
+                                                        <img alt="avatar" src="{{ asset($note->user->photo) }}" class="img-fluid rounded-circle" style="width: 50px; margin-right: 5px;">
                                                     </div>
-                                                    <div data-v-b64104d7="" class="modal-body"><strong data-v-b64104d7=""
-                                                            class="modal-text">Have you Complete The Meeting ?</strong></div>
-                                                    <div data-v-b64104d7="" class="modal-footer d-flex justify-content-center">
-                                                        <button data-v-b64104d7="" class="btn btn-sm" data-dismiss="modal"><i
-                                                                data-v-b64104d7="" class="flaticon-cancel-12"></i> No
-                                                        </button><button data-v-b64104d7="" type="button"
-                                                            class="btn btn-warning btn-sm">Yes</button></div>
+                                                    <div class="media-body">
+                                                        <h6 class="tx-inverse">{{ (!empty($note->user->name))?$note->user->name:'' }}
+                                                            <a onclick="deleteMeetingNote({{ $note->id }})" style="float:right; color:#b30b39;" href="javascript:void(0);" class="action-btn btn-delete bs-tooltip" data-toggle="tooltip" data-placement="top" title="" data-bs-original-title="Delete" aria-label="Delete"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></a>
+                                                            <a id="meeting_waiting{{ $note->id }}" onclick="isMeetingComplete({{ $note->id }})" style="float:right; color:#ada310; margin-right:5px;" href="javascript:void(0);" class="action-btn btn-delete bs-tooltip" data-toggle="tooltip" data-placement="top" title="" data-bs-original-title="Delete" aria-label="Delete"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clock"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg></a>
+                                                            <a id="meeting_complete{{ $note->id }}" onclick="isMeetingComplete({{ $note->id }})" style="float:right; color:#1f6b08; margin-right:5px;" href="javascript:void(0);" class="action-btn btn-delete bs-tooltip" data-toggle="tooltip" data-placement="top" title="" data-bs-original-title="Delete" aria-label="Delete"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-circle"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg></a>
+                                                        </h6>
+                                                        <p class="mg-b-0">{{ (!empty($note->meeting_notes))?$note->meeting_notes:'' }}</p>
+                                                        <small class="text-left"> Meeting Date : <span class="badge badge-warning">June 29 2023 18:33:00</span></small><br>
+                                                        <small class="text-left"> Created : {{ date('F d Y H:i:s',strtotime($note->created_at)) }}</small></div>
                                                 </div>
+                                            </div><hr>
+                                            @endforeach
+                                        @endif
+                                    </div>
+                                    <div class="card-body custom-card-body">
+                                        <form id="meeting-form" method="post">
+                                            <div class="col col-md-12 p-0">
+                                                <div class="form-group lead-drawer-form">
+                                                    <input type="hidden" value="{{ (!empty($application_info->id))?$application_info->id:'' }}" name="meeting_application_id" id="meeting_application_id" />
+                                                    <textarea id="application_meeting" name="application_meeting" class="form-control" rows="2"></textarea>
+                                                </div><br>
+                                                <div class="col col-md-12 p-0">
+                                                    <input name="meeting_date" id="meeting_date" type="datetime-local" class="form-control" />
+                                                </div>
+                                                <button id="btn-meeting-submit" class="btn badge badge-info btn-sm _effect--ripple waves-effect waves-light" >Save</button>
                                             </div>
-                                        </div>
+                                            <hr>
+                                        </form>
+
                                     </div>
                                 </div>
                             </div>
@@ -201,97 +215,47 @@
                                     </tbody>
                                 </table>
                             </div>
-                            <div class="col col-md-12"></div>
-                            <h5 class="pb-2">University Information</h5>
-                            <div class="table-responsive">
-                                <table class="table-bordered mb-4 table">
-                                    <thead>
-                                        <tr>
-                                            <th>Institute Name</th>
-                                            <th>Course Name</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>Bangor University</td>
-                                            <td>BSc (Hons) Pharmacology</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
+
                             <div class="card no-outer-spacing no-border-custom">
                                 <div class="card-header">
                                     <section class="mb-0 mt-0">
                                         <h5>Follow up</h5>
                                     </section>
                                 </div>
-                                <div class="application-follow-up-wrap">
-                                    <div class="card-body custom-card-body p-0">
-                                        <form>
-                                            <div class="col col-md-12 p-0">
-                                                <div class="form-group lead-drawer-form"><label>Follow up Date</label>
-
+                                <div id="" class="" aria-labelledby="headingTwo2" data-parent="#withoutSpacing">
+                                    <div id="followupnote-data">
+                                        @if(count($application_info->followups) > 0)
+                                            @foreach ($application_info->followups as $note)
+                                            <div class="row col-md-12 mt-3" id="">
+                                                <div style="margin-left:7px;" class="media custom-media-img">
+                                                    <div style="margin-left: 2px;" class="ml-2">
+                                                        <img alt="avatar" src="{{ asset($note->user->photo) }}" class="img-fluid rounded-circle" style="width: 50px; margin-right: 5px;">
+                                                    </div>
+                                                    <div class="media-body">
+                                                        <h6 class="tx-inverse">{{ (!empty($note->user->name))?$note->user->name:'' }}<a onclick="deleteFollowupNote({{ $note->id }})" style="float:right; color:#b30b39;" href="javascript:void(0);" class="action-btn btn-delete bs-tooltip" data-toggle="tooltip" data-placement="top" title="" data-bs-original-title="Delete" aria-label="Delete"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></a></h6>
+                                                        <p class="mg-b-0">{{ (!empty($note->follow_up))?$note->follow_up:'' }}</p>
+                                                        <small class="text-left"> Meeting Date : <span class="badge badge-warning">{{ date('F d Y H:i:s',strtotime($note->follow_up_date_time)) }}</span></small><br>
+                                                        <small class="text-left"> Created : {{ date('F d Y H:i:s',strtotime($note->created_at)) }}</small></div>
                                                 </div>
+                                            </div><hr>
+                                            @endforeach
+                                        @endif
+                                    </div>
+                                    <div class="card-body custom-card-body">
+                                        <form id="followup-form" method="post">
+                                            <div class="col col-md-12 p-0">
                                                 <div class="form-group lead-drawer-form">
-                                                    <textarea name="follow_up" cols="30" rows="3" placeholder="Follow up notes..." class="form-control"></textarea>
-                                                    <!---->
-                                                </div><button class="btn badge badge-info btn-sm">Save
-                                                    <!---->
-                                                </button>
+                                                    <input type="hidden" value="{{ (!empty($application_info->id))?$application_info->id:'' }}" name="followup_application_id" id="followup_application_id" />
+                                                    <textarea name="application_followup" id="application_followup" class="form-control" rows="2"></textarea>
+                                                </div><br>
+                                                <div class="col col-md-12 p-0">
+                                                    <input name="followup_date" id="followup_date" type="datetime-local" class="form-control" />
+                                                </div>
+                                                <button id="btn-followup-submit" class="btn badge badge-info btn-sm _effect--ripple waves-effect waves-light" >Save</button>
                                             </div>
                                             <hr>
-                                            <div class="col col-md-12 p-0">
-                                                <ul class="list-group list-group-media drawer-follow-up-list"></ul>
-                                            </div>
-                                        </form><br>
-                                        <div class="row">
-                                            <div class="col col-md-12">
-                                                <div class="lms-pagination">
-                                                    <div data-v-1ef4d76c="" class="flex justify-center custom-pagination">
-                                                        <ul data-v-1ef4d76c="" class="link-list">
-                                                            <li data-v-1ef4d76c=""
-                                                                class="flex border-r border-gray-200 last:border-r-0 dark:border-slate-500">
-                                                                <a data-v-1ef4d76c="" aria-current="page"
-                                                                    href="/application-lead-info/bhe-140581#"
-                                                                    class="router-link-active router-link-exact-active link link-disabled">«
-                                                                    Previous</a></li>
-                                                            <li data-v-1ef4d76c=""
-                                                                class="flex border-r border-gray-200 last:border-r-0 dark:border-slate-500">
-                                                                <a data-v-1ef4d76c="" aria-current="page"
-                                                                    href="/application-lead-info/bhe-140581?page=1"
-                                                                    class="router-link-active router-link-exact-active link hover:bg-gray-200 dark:hover:bg-blue-500 dark:hover:text-white link-active">1</a>
-                                                            </li>
-                                                            <li data-v-1ef4d76c=""
-                                                                class="flex border-r border-gray-200 last:border-r-0 dark:border-slate-500">
-                                                                <a data-v-1ef4d76c="" aria-current="page"
-                                                                    href="/application-lead-info/bhe-140581#"
-                                                                    class="router-link-active router-link-exact-active link link-disabled">Next
-                                                                    »</a></li>
-                                                        </ul>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div data-v-787d8446="" id="followupConfirmationModal" class="modal confirmation-custom fade"
-                                    tabindex="-1" role="dialog" aria-labelledby="followupConfirmationModalLabel"
-                                    aria-hidden="true">
-                                    <div data-v-787d8446="" class="modal-dialog" role="document">
-                                        <div data-v-787d8446="" class="modal-content">
-                                            <div data-v-787d8446="" class="modal-header">
-                                                <h5 data-v-787d8446="" id="followupConfirmationModalLabel" class="modal-title">
-                                                    Confirmation</h5><button data-v-787d8446="" type="button" class="close"
-                                                    data-dismiss="modal" aria-label="Close"></button>
-                                            </div>
-                                            <div data-v-787d8446="" class="modal-body"><strong data-v-787d8446=""
-                                                    class="modal-text">Have you Complete The Follow up ?</strong></div>
-                                            <div data-v-787d8446="" class="modal-footer d-flex justify-content-center"><button
-                                                    data-v-787d8446="" class="btn btn-sm" data-dismiss="modal"><i
-                                                        data-v-787d8446="" class="flaticon-cancel-12"></i> No </button><button
-                                                    data-v-787d8446="" type="button" class="btn btn-warning btn-sm">Yes</button>
-                                            </div>
-                                        </div>
+                                        </form>
+
                                     </div>
                                 </div>
                             </div>
@@ -318,7 +282,7 @@
         color: #a90606 !important;
     }
     .custom-media-margin{
-        margin: 7px !important;
+        margin: 9px !important;
     }
 </style>
 @stop
