c95335ddf1e85384c856641e17c635c73acb5c8d
khantanvir <aiub.tanvir@gmail.com>
2023-07-20 20:29:35 +0600

    wert
    Signed-off-by: khantanvir <aiub.tanvir@gmail.com>

--- resources/views/application/meeting_details.blade.php
+++ resources/views/application/meeting_details.blade.php
@@ -121,7 +121,7 @@
                                     @foreach ($documents as $doc)
                                     <tr>
                                         <td>{{ $doc->title }}</td>
-                                        <td><a target="_blank" href="{{ asset($doc->document) }}">Download Here</a></td>
+                                        <td><a target="_blank" href="{{ asset($doc->document) }}">Preview Here</a></td>
                                         <td class="text-center">
                                             <a href="javascript:void(0)" onclick="if(confirm('Are you sure to Delete this Document?')) location.href='{{ URL::to('meeting-document-delete/'.$doc->id) }}'; return false;" style="color:#b30b39 !important;" href="javascript:void(0);" class="action-btn btn-delete bs-tooltip" data-toggle="tooltip" data-placement="top" title="" data-bs-original-title="Delete" aria-label="Delete">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
