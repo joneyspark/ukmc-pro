f6183c15db61d648f9bb9e32c17a453c6cc8a562
khantanvir <aiub.tanvir@gmail.com>
2023-12-21 21:49:29 +0600

    ccccc
    Signed-off-by: khantanvir <aiub.tanvir@gmail.com>

--- app/Models/Course/CourseIntake.php
+++ app/Models/Course/CourseIntake.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CourseIntake extends Model{
+
     use HasFactory;
     public $table = "course_intakes";
 }
