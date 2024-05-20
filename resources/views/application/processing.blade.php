5cb3c1bad34ee9ef455f35e43302dfa2f187f3a9
Tanvir <aiub.tanvir@gmail.com>
2024-05-20 17:08:16 +0600

    dddd
    Signed-off-by: Tanvir <aiub.tanvir@gmail.com>

--- resources/views/application/processing.blade.php
+++ resources/views/application/processing.blade.php
@@ -452,8 +452,14 @@
                                         <tr>
                                             <td>{{ $doc->document_type }}</td>
                                             <td>
+                                                @if(!empty($doc->create_date))
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
+                                                <span class="table-inner-text">{{ date('F d Y',strtotime($doc->create_date)) }}</span>
+                                                @else
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
                                                 <span class="table-inner-text">{{ date('F d Y',strtotime($doc->created_at)) }}</span>
+                                                @endif
+
                                             </td>
                                             <td>
                                                 <a target="_blank" href="{{ asset($doc->doc) }}"><span class="badge badge-light-success">Preview</span></a>
